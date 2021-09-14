3477bf67b4b9ce27d9ab9f77e766ad212d420f91
Nima-Faraji <55926070+Nima-Faraji@users.noreply.github.com>
2021-09-13 17:33:26 -0700

    new web

---
+++ js/chunk-2d0e9374.bffa25d6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e9374"],{"8d1c":function(e,t,a){"use strict";a.r(t);var n,o,s=a("0dfe"),i=a("6512"),u={name:"Home",metaInfo:{title:"Home"},extends:s["a"],mixins:[Object(i["a"])(["hero","theme-features","features","affiliates","social-media","pro-features"])],props:{id:{type:String,default:"home"}}},c=u,l=a("0c7c"),r=Object(l["a"])(c,n,o,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0e9374.bffa25d6.js.map
